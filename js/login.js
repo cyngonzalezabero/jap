c7d9e3f1750b3e754c833d5c5d25d1dc9b09f36b
cyngonzalezabero <cgonzalezabero@gmail.com>
2021-08-29 18:47:32 -0300

    Entrega 1 final

--- js/login.js
+++ js/login.js
@@ -1,6 +1,27 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+function login() {
+    let usuario = document.getElementById("usuario").value 
+    console.log(usuario)
+    let clave = document.getElementById("clave").value
+    console.log(clave)
+
+    if (usuario == '' || clave == '') { 
+        document.getElementById("alerta").innerHTML=`
+        <div class="alert alert-danger" role="alert">
+        Campo vacio!
+        </div>
+        <br>
+        <br>`
+        
+    }
+        else { 
+            window.location.href = "home.html"
+        }
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,38 @@
+//hacer peticion
+async function mostrarProductos() {
+    let promesa = await getJSONData(PRODUCTS_URL)
+
+let productos = promesa.data
+productos.forEach(producto => {
+    let contenedor = document.getElementById("listado-productos")
+    contenedor.innerHTML += `
+    <a href="product-info.html" class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col-3">
+                <img src="${producto.imgSrc}" alt="${producto.description}" class="img-thumbnail">
+            </div>
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1">${producto.name}</h4>
+                    <small class="text-muted">${producto.soldCount} artículos</small>
+                </div>
+                <p class="mb-1">${producto.description}</p>
+                <div class="text-right mt-3"><p>${producto.currency+producto.cost}</p></div>
+            </div>
+        </div>
+    </a>
+    `
+});
+}
+
+
+
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
+mostrarProductos()
 });
